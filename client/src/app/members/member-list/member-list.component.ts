75225e91ae256d54aeccd5468f2adb63a9df7af4
vincent <vincent.arteaga@gmail.com>
2022-01-17 22:27:31 -0800

    end of section 6

---
+++ client/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list',
+  template: `
+    <p>
+      member-list works!
+    </p>
+  `,
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 // import * as EventEmitter from 'events';
 
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toaster: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +24,7 @@ export class RegisterComponent implements OnInit {
       this.cancel()
     }, error => {
       console.log(error)
+      this.toaster.error(error.error)
     })
   }
 
