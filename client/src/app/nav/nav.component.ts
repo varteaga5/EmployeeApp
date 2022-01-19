4d7a0983547915fe3d0bdeef158d3d6df9ac66fe
vincent <vincent.arteaga@gmail.com>
2022-01-18 22:03:31 -0800

    end of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -20,12 +20,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       (response) => {
         this.router.navigateByUrl('/members');
-      },
-      (error) => {
-        console.log(error);
-        this.toastr.error(error.error)
-      }
-    );
+      });
   }
 
   logout() {
