0ea25252c7b398e43fbf2bf8801f6d6b611860b3
vincent <vincent.arteaga@gmail.com>
2022-01-26 23:53:43 -0800

    expanding register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 // import * as EventEmitter from 'events';
@@ -12,20 +13,43 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup;
 
-  constructor(private accountService: AccountService, private toaster: ToastrService) { }
+  constructor(private accountService: AccountService, private toaster: ToastrService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+      this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    })
+
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(res => {
-      console.log(res)
-      this.cancel()
-    }, error => {
-      console.log(error)
-      this.toaster.error(error.error)
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(res => {
+    //   console.log(res)
+    //   this.cancel()
+    // }, error => {
+    //   console.log(error)
+    //   this.toaster.error(error.error)
+    // })
   }
 
   cancel() {
