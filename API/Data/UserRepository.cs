dad04cd3e217253684937a14cc193b9972001417
vincent <vincent.arteaga@gmail.com>
2022-01-29 00:08:36 -0800

    adding action filter

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -38,6 +38,12 @@ namespace API.Data
 
                 query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
+                query = userParams.OrderBy switch
+                {
+                    "Created" => query.OrderByDescending(u => u.Created),
+                    _ => query.OrderByDescending(u => u.LastActive)
+                };
+
                 return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(), userParams.PageNumber, userParams.PageSize);
         }
 
