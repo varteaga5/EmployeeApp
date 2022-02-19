856053e5bde8ead17801c94f05d6d623da0cb860
vincent <vincent.arteaga@gmail.com>
2022-02-19 12:21:00 -0800

    displaying online presence

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -11,6 +11,7 @@ import { Member } from 'src/app/_models/member';
 import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -26,7 +27,7 @@ export class MemberDetailComponent implements OnInit {
   messages: Message[] = [];
  
   constructor(
-    private memberService: MembersService,
+    public presence: PresenceService,
     private route: ActivatedRoute,
     private messageService: MessageService
   ) {}
