db60bc66f8a1a1ae8504151641e9bfb6a4d16d31
vincent <vincent.arteaga@gmail.com>
2022-02-20 20:12:12 -0800

    Getting the data from the confirmation modal

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean {
+
+  constructor(private ConfirmService: ConfirmService) {}
+
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
     // checks if any updates and prevents accidental changing of pages
     if (component.editForm.dirty) {
-      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
+      return this.ConfirmService.confirm()
     }
     return true;
   }
